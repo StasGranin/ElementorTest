f982185c38048ca084ddb0eac7413aaadc5ab0d6
Stas <sgranin@gmail.com>
2020-09-30 14:20:08 +0300

    Updated the abstract ui component class: Added automatic ui DOM elements selection by "ui" attribute and lifecycle functions

--- src/client/modules/uiComponent/uiComponent.js
+++ src/client/modules/uiComponent/uiComponent.js
@@ -4,17 +4,57 @@
 
 export default class UIComponent {
 
-	constructor(appState, templateHTML) {
+	constructor(state, templateHTML) {
 
 		this.appState = appState;
-		this.state = {};
+		this.state = state || {};
+		this.$element = null;
+		this.uiElements = {};
 
 		if (templateHTML) {
-			this.$element = $(templateHTML);
+			const $element = $(templateHTML);
+
+			$('[ui]', $element).each((index, element) => {
+				const uiElementName = element.getAttribute('ui');
+
+				this.uiElements[uiElementName] = $(`[ui="${uiElementName}"]`, $element);
+			});
+
+			this.$element = $element;
 		}
 	}
 
-	render($parent) {
+	onAppend() {
+	}
+
+	onUpdate(state) {
+	}
+
+	onRemove() {
+	}
+
+	remove() {
+		this.$element && this.$element.remove();
+		this.onRemove();
+
+		return this;
+	}
+
+	update(state) {
+		if (state) {
+			this.state = state;
+		}
+
+		this.onUpdate(state);
+
+		return this;
+	}
+
+	appendTo($parent) {
 		$parent.append(this.$element);
+		this.onUpdate(this.state);
+		this.onAppend();
+
+		return this;
 	}
 }
