6ad07930873e8bbd8b8eac15747eea3690192ab0
Stas <sgranin@gmail.com>
2020-09-30 22:38:01 +0300

    Modified the exports to be objects. Why? I dunno, it's 10:30, I have nothing better to do

--- src/server/services/auth.service.js
+++ src/server/services/auth.service.js
@@ -5,22 +5,23 @@ const jwt = require("jsonwebtoken");
 const APP_TOKEN_SECRET = process.env.APP_TOKEN_SECRET || 'elementor';
 const APP_TOKEN_EXPIRE = process.env.APP_TOKEN_EXPIRE || 86400; // Default: expire in 24 hours
 
-module.exports.makeToken = (username) => jwt.sign({username}, APP_TOKEN_SECRET, {expiresIn: APP_TOKEN_EXPIRE});
+module.exports = {
+	makeToken: (username) => jwt.sign({username}, APP_TOKEN_SECRET, {expiresIn: APP_TOKEN_EXPIRE}),
 
-// Verify token middleware
-module.exports.verifyToken = (req, res, next) => {
-	const token = req.headers['x-access-token'];
+	verifyToken: (req, res, next) => {
+		const token = req.headers['x-access-token'];
 
-	if (!token) {
-		return res.status(403).send({data: {errorMessage: 'No token provided!'}});
-	}
-
-	jwt.verify(token, APP_TOKEN_SECRET, (err, decoded) => {
-		if (err) {
-			return res.status(401).send({data: {errorMessage: 'Unauthorized!'}});
+		if (!token) {
+			return res.status(403).send({data: {errorMessage: 'No token provided!'}});
 		}
 
-		//req.userId = decoded.id;
-		next();
-	});
+		jwt.verify(token, APP_TOKEN_SECRET, (err, decoded) => {
+			if (err) {
+				return res.status(401).send({data: {errorMessage: 'Unauthorized!'}});
+			}
+
+			//req.userId = decoded.id;
+			next();
+		});
+	}
 };
--- src/server/services/database.service.js
+++ src/server/services/database.service.js
@@ -2,13 +2,18 @@
 
 const {MongoClient} = require('mongodb'); // vanilla MongoDB driver. No Mongoose for you, come back next year!
 
-let _db;
+module.exports = {
+	_db: null,
 
-module.exports.connect = (dbURL, dbName, callback) => {
-	MongoClient.connect(dbURL, {useUnifiedTopology: true}).then((mongoClient) => {
-		_db = mongoClient.db(dbName);
-		callback();
-	}).catch(error => console.error(error));
+	connect: (dbURL, dbName, callback) => {
+		MongoClient.connect(dbURL, {useUnifiedTopology: true}).then((mongoClient) => {
+			this._db = mongoClient.db(dbName);
+			callback();
+		}).catch(error => console.error(error));
+	},
+
+	getDatabase: () => this._db,
+
+	getCollection: (collectionName) => this._db.collection(collectionName)
+
 };
-
-module.exports.getDatabase = () => _db;
