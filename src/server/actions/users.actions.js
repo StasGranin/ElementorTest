dd3e29392fd0f86dfd1631d73fdd886e551f1469
Stas <sgranin@gmail.com>
2020-10-01 18:48:29 +0300

    Created the getActiveUsers function

--- src/server/actions/users.actions.js
+++ src/server/actions/users.actions.js
@@ -1,9 +1,30 @@
 "use strict";
 
 const {getCollection} = require('../services/database.service');
+const {send, sendError, sendGenericError} = require('../services/sendToClient.service');
+const {getClientIP, getClientUserAgent} = require('../services/util.services');
 
 module.exports = {
 	getActiveUsers: (req, res) => {
+		const activeUsersCollection = getCollection('activeUsers');
+		const {sid, username} = req.userSession;
+
+		try {
+			activeUsersCollection.updateOne({_id: sid, username}, {
+				$set: {
+					lastUpdateTime: new Date().getTime(),
+					userAgent: getClientUserAgent(req),
+					userIP: getClientIP(req)
+				}
+			}).then(result => {
+				activeUsersCollection.find({}).toArray().then((results) => {
+					send(res, 200, results);
+				})
+			});
+		}
+		catch (error) {
+			sendGenericError(res);
+		}
 	},
 
 	getUserDetails: (req, res) => {
