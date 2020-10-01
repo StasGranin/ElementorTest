bb84c42eb052ea682ac455fd2de88b012b5fb1cb
Stas <sgranin@gmail.com>
2020-10-01 16:16:48 +0300

    Modified auth to accept multiple sessions from the same user,  added try-catch to critical areas and replaced all send functionality with service functions

--- src/server/actions/auth.actions.js
+++ src/server/actions/auth.actions.js
@@ -3,6 +3,8 @@
 const crypto = require('crypto');
 const {getCollection} = require('../services/database.service');
 const {makeToken, verifyToken, decodeToken} = require('../services/auth.service');
+const {send, sendError, sendGenericError} = require('../services/sendToClient.service');
+const {getClientIP, getClientUserAgent} = require('../services/util.services');
 
 
 module.exports = {
@@ -11,13 +13,13 @@ module.exports = {
 		const password = req.body.password && req.body.password.trim();
 
 		if (username && password) {
-			const collection = getCollection('users');
+			try {
+				const collection = getCollection('users');
 
-			collection.findOne({username}).then(user => {
-				const passwordHash = crypto.createHash('sha1').update(password).digest("hex"); // I decided not to use salt for hashing. Yes, I'm aware of the implications
+				collection.findOne({username}).then(user => {
+					const passwordHash = crypto.createHash('sha1').update(password).digest("hex"); // I decided not to use salt for hashing. Yes, I'm aware of the implications
 
-				if (!user) {
-					try {
+					if (!user) {
 						collection.insertOne({
 							username,
 							passwordHash,
@@ -25,17 +27,17 @@ module.exports = {
 							signUpTime: new Date().getTime()
 						}).then(result => res.status(201).send());
 					}
-					catch (err) {
-						res.status(500).send({data: {errorMessage: 'User creation failed'}});
+					else {
+						sendError(res, 400, 'Username already taken');
 					}
-				}
-				else {
-					res.status(400).send({data: {errorMessage: 'Username already exists'}});
-				}
-			});
+				});
+			}
+			catch (error) {
+				sendGenericError(res);
+			}
 		}
 		else {
-			res.status(400).send({data: {errorMessage: 'Missing username or password'}});
+			sendError(res, 400, 'Missing username or password');
 		}
 	},
 
@@ -44,63 +46,70 @@ module.exports = {
 		const password = req.body.password && req.body.password.trim();
 
 		if (username && password) {
-			const now = new Date().getTime();
-			const usersCollection = getCollection('users');
-			const activeUsersCollection = getCollection('activeUsers');
-			const passwordHash = crypto.createHash('sha1').update(password).digest("hex");
-			const updateUserPromise = usersCollection.updateOne({username, passwordHash}, {
-				$inc: {loginCount: 1},
-				$set: {lastLoginTime: now}
-			});
-			const findUserInActiveUsersPromise = activeUsersCollection.findOne({username}); // In case user is already logged in (say on a different machine/browser)
+			try {
+				const now = new Date().getTime();
+				const usersCollection = getCollection('users');
+				const activeUsersCollection = getCollection('activeUsers');
+				const passwordHash = crypto.createHash('sha1').update(password).digest("hex");
 
-			Promise.all([updateUserPromise, findUserInActiveUsersPromise]).then(results => {
-				const [user, activeUser] = results;
-				const userIP = (req.ip === '::1' || req.ip === '127.0.0.1') ? 'localhost' : req.ip; // IPv6 localhost string is weird
+				usersCollection.updateOne({username, passwordHash}, {
+					$inc: {loginCount: 1},
+					$set: {lastLoginTime: now}
+				}).then(result => {
 
-				if (user.matchedCount) {
-					let updateActiveUserPromise;
-					const activeUserData = {
-						username,
-						lastUpdateTime: now,
-						loginTime: now,
-						userAgent: req.get('user-agent'),
-						userIP
-					};
+					if (result.matchedCount) {
+						activeUsersCollection.insertOne({
+							username,
+							lastUpdateTime: now,
+							loginTime: now,
+							userAgent: getClientUserAgent(req),
+							userIP: getClientIP(req)
+						}).then(result => {
+							const sid = result.ops[0]._id;
+							const accessToken = makeToken(sid, username);
 
-					if (activeUser) {
-						updateActiveUserPromise = activeUsersCollection.updateOne({username}, {$set: activeUserData});
+							send(res, 200, {sid, username, accessToken});
+						})
 					}
 					else {
-						updateActiveUserPromise = activeUsersCollection.insertOne(activeUserData);
+						sendError(res, 400, 'Incorrect username or password');
 					}
-
-					updateActiveUserPromise.then((result) => {
-						const accessToken = makeToken(username);
-
-						res.status(200).send({data: {username, accessToken}});
-					});
-				}
-				else {
-					res.status(400).send({data: {errorMessage: 'Username or password are incorrect'}});
-				}
-			});
+				});
+			}
+			catch (error) {
+				sendGenericError(res);
+			}
 		}
 		else {
-			res.status(400).send({data: {errorMessage: 'Missing username or password'}});
+			sendError(res, 400, 'Missing username or password');
 		}
 	},
 
 	logOut: (req, res) => {
-		const {username} = decodeToken(req) || {};
+		const {sid, username} = decodeToken(req) || {};
 
 		if (username) {
-			getCollection('activeUsers').deleteOne({username}).then((result) => {
-				res.status(201).send();
-			});
+			try {
+				getCollection('activeUsers').deleteOne({_id: sid, username}).then((result) => {
+					send(res, 200);
+				});
+			}
+			catch (error) {
+				sendGenericError(res);
+			}
+
 		}
 		else {
-			res.status(400).send({data: {errorMessage: 'Failed to log-out properly! Supplied credentials are incorrect'}});
+			sendError(res, 400, 'Failed to log-out properly! Supplied credentials are incorrect');
+		}
+	},
+
+	silentlyLogOut: (username, sid) => {
+		try {
+			getCollection('activeUsers').deleteOne({_id: sid, username});
+		}
+		catch (error) {
+			sendGenericError(res);
 		}
 	}
 };
--- src/server/services/auth.service.js
+++ src/server/services/auth.service.js
@@ -7,27 +7,29 @@ const {sendError} = require('./sendToClient.service');
 const APP_TOKEN_SECRET = process.env.APP_TOKEN_SECRET || 'elementor';
 
 module.exports = {
-	makeToken: (username) => jwt.sign({username}, APP_TOKEN_SECRET),
+	makeToken: (sid, username) => jwt.sign({sid, username}, APP_TOKEN_SECRET),
 
 	verifyToken: (req, res, next) => {
 		const token = req.headers['x-access-token'];
 
 		if (token) {
-			jwt.verify(token, APP_TOKEN_SECRET, (err, decoded) => {
+			jwt.verify(token, APP_TOKEN_SECRET, (err, decoded = {}) => {
+				const {sid, username} = decoded;
+
 				if (err) {
-					if (decoded.username) {
-						silentlyLogOut(decoded);
+					if (sid && username) {
+						silentlyLogOut(sid, username);
 					}
 
 					return sendError(401, 'Invalid token');
 				}
 
-				req.username = decoded.username;
+				req.userSession = {sid, username};
 				next();
 			});
 		}
 		else {
-			return sendError(403, 'No token provided!');
+			return sendError(401, 'No token provided!');
 		}
 
 	},
