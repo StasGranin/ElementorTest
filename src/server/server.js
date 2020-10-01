b3f0453fa47dc0658b7f231b590fba0f24ae38b4
Stas <sgranin@gmail.com>
2020-10-01 16:15:01 +0300

    Created util functions service and added "trust-proxy" configuration to server

--- src/server/server.js
+++ src/server/server.js
@@ -15,6 +15,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.set('trust proxy', true);
 
 app.use(express.static(path.join(__dirname, '../../public')));
 app.use(express.static(path.join(__dirname, '../../dist')));
---
+++ src/server/services/util.services.js
@@ -0,0 +1,5 @@
+"use strict";
+module.exports = {
+	getClientIP: (req) => (req.ip === '::1' || req.ip === '127.0.0.1') ? 'localhost' : req.ip, // IPv6 localhost string is weird
+	getClientUserAgent: (req) => req.get('user-agent')
+};
